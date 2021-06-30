f48e42a0740048d59f9608a5b01d4a7be8c27deb
11200410092019 <86745942+11200410092019@users.noreply.github.com>
2021-07-01 04:07:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Cục cưng à :3 :3 ',
+    introDesc: `Vào một ngày nắng hạ,
+    Trong tim a có em.
+    Muốn nói với em rằng
+    Yêu em hơn 3000 <3 :3
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
